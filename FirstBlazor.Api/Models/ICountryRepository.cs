d8ee91f27c10c615ae070f8cd0158bebfa1ebddf
MohamedHlabba <mohamed.hlabba@esprit.tn>
2021-05-21 19:08:33 +0200

    add API

---
+++ FirstBlazor.Api/Models/ICountryRepository.cs
@@ -0,0 +1,14 @@
+﻿using FirstBlazorWebAssebly.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstBlazor.Api.Models
+{
+    public interface ICountryRepository
+    {
+        IEnumerable<Country> GetAllCountries();
+        Country GetCountryById(int countryId);
+    }
+}
---
+++ FirstBlazor.Api/Models/IJobCategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using FirstBlazorWebAssebly.Shared;
+using System.Collections.Generic;
+
+namespace FirstBlazor.Api.Models
+{
+    public interface IJobCategoryRepository
+    {
+        IEnumerable<JobCategory> GetAllJobCategories();
+        JobCategory GetJobCategoryById(int jobCategoryId);
+    }
+}

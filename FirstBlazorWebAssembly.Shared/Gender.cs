e17497cd8a728e20d88a8e837c38010791bab797
MohamedHlabba <mohamed.hlabba@esprit.tn>
2021-05-21 14:26:24 +0200

    Add project files.

---
+++ FirstBlazorWebAssembly.Shared/Gender.cs
@@ -0,0 +1,9 @@
+﻿namespace FirstBlazorWebAssebly.Shared
+{
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other
+    }
+}
---
+++ FirstBlazorWebAssembly.Shared/MaritalStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace FirstBlazorWebAssebly.Shared
+{
+    public enum MaritalStatus
+    {
+        Married,
+        Single,
+        Other
+    }
+}
